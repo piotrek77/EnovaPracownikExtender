f6adaed2f56a13171e9282216afd1bdee9403c4d
Piotr Osłowski <piotr.oslowski@scapaflow.pl>
2023-04-04 17:03:37 +0200

    INIT

---
+++ EnovaPracownikExtender/ScpPracExtTable.cs
@@ -0,0 +1,36 @@
+﻿using Soneta.Handel;
+using Soneta.Kadry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnovaPracownikExtender
+{
+    public class ScpPracExtTable : EnovaPracownikExtender.ScpPracownikExtenderModule.ScpPracownikExtenderTable
+    {
+
+
+        static ScpPracExtTable()
+        {
+            //Soneta.Handel.HandelModule.DokumentHandlowySchema.AddOnAdded(new Soneta.Business.RowDelegate<Soneta.Handel.HandelModule.DokumentHandlowyRow>(DokumentHandlowyAdded));
+            Soneta.Kadry.KadryModule.PracownikSchema.AddOnAdded(new Soneta.Business.RowDelegate<Soneta.Kadry.KadryModule.PracownikRow>(PracownikAdded));
+
+        }
+
+
+
+        static void PracownikAdded(Soneta.Kadry.KadryModule.PracownikRow pracownikRow)
+        {
+            Pracownik pracownik = (Pracownik)pracownikRow;
+
+            ScpPracownikExtender scpPracownikExtender = new ScpPracownikExtender()
+            {
+                Host = pracownik,
+            };
+            ScpPracownikExtenderModule.GetInstance(pracownik).ScpPracExtTable.AddRow(scpPracownikExtender);
+            
+        }
+
+
+    }
+}
---
+++ EnovaPracownikExtender/ScpPracownikExtender.cs
@@ -0,0 +1,25 @@
+﻿using Soneta.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnovaPracownikExtender
+{
+    public class ScpPracownikExtender : EnovaPracownikExtender.ScpPracownikExtenderModule.ScpPracownikExtenderRow
+    {
+
+
+        protected override void OnDeleting()
+        {
+            base.OnDeleting();
+            RowStatus status = Host.Status;
+
+            if ((status & RowStatus.Deleting) == 0 && !Session.InImport)
+            {
+                throw new RowException(this, "Dodatkowe informacje o pracowniku mogą być kasowe tylko z zapisem głównym");
+            }
+        }
+
+
+    }
+}
